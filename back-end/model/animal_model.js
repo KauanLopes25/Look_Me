fd34550537514fb7234e32e89e4b7ccbce438941
KAUAN LOPES PEREIRA <25132925@CFP127ALUNO.LOCAL>
2025-12-03 07:47:08 -0300

    MODEL
    Adição de um arquivo model para a tabela de animais

---
+++ back-end/model/animal_model.js
@@ -0,0 +1,28 @@
+/********************************************************************************************
+* Objetivo: Arquivo para comunicação com a tabela animal.
+* Autor: 
+* Data: 
+* Versão: 1.0
+********************************************************************************************/
+
+/* Comentário em bloco */
+// Comentário em linha
+
+/********************************************************************************************
+********************************* COMANDOS UTILIZADOS ***************************************
+* $queryRawUnsafe(script) -> permite executar um script SQL de uma variavel e que retorna 
+valores de um banco (SELECT).
+* $executeRawUnsafe(script) -> Permite executar um script SQL de uma variavel e que não 
+retorna dados do banco (INSERT, UPDATE e DELETE.)
+* $queryRaw(script) -> permite executar um script SQL sem estar em uma variavel e que retorna 
+valores do banco (SELECT). Faz tratamentos de segurança contra SQL Injection
+* $executeRaw(script) -> Permite executar um script SQL sem estar em uma variavel e que não 
+retorna dados do banco (INSERT, UPDATE e DELETE.). Faz tratamentos de segurança contra SQL 
+Injection
+************************************** OBSERVAÇÕES ******************************************
+* Todo array possui um indice, e para acessar um indice devemos dizer qual a sua posição [0],
+[3] ou [n], seria a posição em que aquele elemento se encontra dentro de todo array.
+* Todo Json pode ser acessado com ".nomeAtributo".
+********************************* EXEMPLOS BIBLIOTECAS **************************************
+******************************** BIBLIOTECAS UTILIZADAS *************************************
+********************************************************************************************/
